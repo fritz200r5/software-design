54bc16246f647397e6f90f0352c48605419d446b
CpELaboratory 216 <CpELaboratory 216@BE216-PC37>
2026-01-22 09:55:14 +0800

    number sorter

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -20,20 +20,69 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Sort = new Button();
+            listBoxResults = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // Sort
+            // 
+            Sort.BackColor = SystemColors.AppWorkspace;
+            Sort.Location = new Point(346, 382);
+            Sort.Name = "Sort";
+            Sort.Size = new Size(102, 36);
+            Sort.TabIndex = 0;
+            Sort.Text = "Sort";
+            Sort.UseVisualStyleBackColor = false;
+            Sort.Click += btnSort_Click;
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.BackColor = SystemColors.InactiveCaption;
+            listBoxResults.ItemHeight = 15;
+            listBoxResults.Location = new Point(195, 108);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(382, 229);
+            listBoxResults.TabIndex = 0;
+            listBoxResults.SelectedIndexChanged += listBoxResults_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label1.Location = new Point(283, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(210, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Number Sorter";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(listBoxResults);
+            Controls.Add(Sort);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button Sort;
+        private ListBox listBoxResults;
+        private Label label1;
+    }
 }
--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -1,10 +1,34 @@
 namespace lab1
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public int[] ProceduralSort(int[] numbers)
+        {
+            Array.Sort(numbers);
+            return numbers;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 3, 1, 5, 2, 4 };
+            int[] sortedNumbers = ProceduralSort(numbers);
+            listBoxResults.DataSource = sortedNumbers;
+        }
+
+        private void listBoxResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
