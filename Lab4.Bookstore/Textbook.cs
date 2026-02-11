2a04e79cf2482d923488e5a6197b3a15d26d16c7
Fritz Pedrosa <fritzjosef013@gmail.com>
2026-02-11 23:56:48 +0800

    update

---
+++ Lab4.Bookstore/Textbook.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab4.Bookstore;
+
+public class Textbook : Book
+{
+    private string _subject = string.Empty;
+
+    public required string Subject
+    {
+        get => _subject;
+        init => _subject = ValidateRequired(value, nameof(Subject));
+    }
+
+    public override string GetInfo() => $"{Title} (Textbook: {Subject}) by {Author}";
+}
---
+++ lab4/BookTests.cs
@@ -0,0 +1,38 @@
+﻿using Lab4.Bookstore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab4.Bookstore.Tests;
+
+[TestClass]
+public class BookTests
+{
+    [TestMethod]
+    public void TestBookCreation()
+    {
+        var book = new Book { Title = "C# Programming", Author = "John Smith" };
+
+        Assert.AreEqual("C# Programming", book.Title);
+        Assert.AreEqual("John Smith", book.Author);
+        Assert.AreEqual("C# Programming by John Smith", book.GetInfo());
+    }
+
+    [TestMethod]
+    public void TestMagazineCreation()
+    {
+        var magazine = new Magazine { Title = "Tech Weekly", Author = "Jane Doe", IssueNumber = 12 };
+
+        Assert.AreEqual("Tech Weekly", magazine.Title);
+        Assert.AreEqual(12, magazine.IssueNumber);
+        Assert.AreEqual("Tech Weekly - Issue 12", magazine.GetInfo());
+    }
+
+    [TestMethod]
+    public void TestEbookCreation()
+    {
+        var ebook = new Ebook { Title = "Learn C#", Author = "Mark Jones", FileSizeMb = 5 };
+
+        Assert.AreEqual("Learn C#", ebook.Title);
+        Assert.AreEqual(5, ebook.FileSizeMb);
+        Assert.AreEqual("Learn C# (Ebook, 5MB)", ebook.GetInfo());
+    }
+}
